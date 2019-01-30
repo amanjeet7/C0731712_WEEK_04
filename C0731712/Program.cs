2ac115615a9a843e5ea41261b918be1fc12f57ee
amanjeet <C0731712@gmail.com>
2019-01-30 11:21:50 -0500

    i addedthe countryside class

--- C0731712/Program.cs
+++ C0731712/Program.cs
@@ -21,7 +21,32 @@ namespace C0731712
         // Node ia an ADT
         // what kind of data do we need in a NODE?
         public village nextvillage;
-            public string villagename;
+        public village previousvillage;
+        public string villagename;
             public bool isastrildehere = false;
+    }
+    class countryside
+    {
+        village maple = new village();
+       
+        village toronto = new village();
+        
+        village ajax = new village();
+        village first;
+        village last;
+
+
+        public void Launch()
+        {
+            maple.villagename = "maple";
+            maple.nextvillage = toronto;
+            maple.previousvillage = null;
+            toronto.villagename = "toonto";
+            toronto.nextvillage = ajax;
+            toronto.previousvillage = maple;
+            ajax.villagename = "ajax";
+            ajax.nextvillage = null;
+            ajax.nextvillage = toronto;
+        } 
     }
 }
